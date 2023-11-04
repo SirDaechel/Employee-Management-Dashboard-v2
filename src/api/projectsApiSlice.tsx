ef1e2c0f92dea265d715ce266362867d3432f598
SirDaechel <anayookpala26@gmail.com>
2023-11-04 22:17:33 +0100

    made fetching json data from github possible

--- src/api/projectsApiSlice.tsx
+++ src/api/projectsApiSlice.tsx
@@ -2,11 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const projectsApiSlice = createApi({
   reducerPath: "projects",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://sirdaechel.github.io/jsonapi-/",
+  }),
   tagTypes: ["Projects"],
   endpoints: (builder) => ({
     getProjects: builder.query({
-      query: () => "/projects",
+      query: () => "projects.json",
       transformResponse: (res: any) => res.slice(0, 6),
       providesTags: ["Projects"],
     }),
